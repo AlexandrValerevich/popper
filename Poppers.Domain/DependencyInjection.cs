89507ccf9a10544c529d00c458e5f351fe58233d
Alexander Nesterovich <sasha.nest92@gmail.com>
2022-07-27 23:41:34 +0300

    Add DI to Domain

---
+++ Poppers.Domain/DependencyInjection.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+using Poppers.Domain.Factory;
+
+namespace Poppers.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        services.AddSingleton<IGifFactory, GifFactory>();
+        return services;
+    }
+}

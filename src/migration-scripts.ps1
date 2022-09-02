69a8558efc14ee3db37aff077f896187882881b7
Alexander <sasha.nest92@gmail.com>
2022-09-02 08:30:04 +0300

    Change migration-script

--- src/migration-scripts.ps1
+++ src/migration-scripts.ps1
@@ -1,5 +1,5 @@
-cd D:\Code\.NET\MeetHead\src\Services\Poppers\Poppers.Api
+cd .\Services\Poppers\Poppers.Api
 
-dotnet ef migrations add "Name" -o d:\Code\.NET\MeetHead\src\Services\Poppers\Poppers.Infrastructure\Persistence\EF\Migrations -s d:\Code\.NET\MeetHead\src\Services\Poppers\Poppers.Api
+# dotnet ef migrations add "Name" -o d:\Code\.NET\MeetHead\src\Services\Poppers\Poppers.Infrastructure\Persistence\EF\Migrations -s d:\Code\.NET\MeetHead\src\Services\Poppers\Poppers.Api
 
 dotnet ef database update
